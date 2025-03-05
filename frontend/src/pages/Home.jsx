4bf8da48494e87fd4b98346ef8f5b3861862ce89
seancuisun <seancuisun@hotmail.com>
2025-03-05 10:59:22 -0800

    second commit

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import {useState, useEffect} from "react";
+import { useState, useEffect } from "react";
 import api from "../api";
 import Note from "../components/Note";
 import "../styles/Home.css"
@@ -37,7 +37,7 @@ function Home() {
     const createNote = (e) => {
         e.preventDefault()
         api
-            .post("/api/notes/", {content, title})
+            .post("/api/notes/", { content, title })
             .then((res) => {
                 if (res.status === 201) alert("Note created!");
                 else alert("Failed to make note.");
